634a1653914478037762f549859a2e99e04d6f5f
yonghao.niu <yonghao.niu@100credit.com>
2021-04-08 19:27:16 +0800

    第四周作业

--- 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
+++ 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
@@ -1,6 +1,8 @@
 package java0.conc0303;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 
 /**
  * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
@@ -10,30 +12,65 @@ import java.util.concurrent.CountDownLatch;
  * 一个简单的代码参考：
  */
 public class Homework03 {
-    
-    public static void main(String[] args) {
-        
-        long start=System.currentTimeMillis();
 
-        // 在这里创建一个线程或线程池，
-        // 异步执行 下面方法
-    
-        int result = sum(); //这是得到的返回值
-        
-        // 确保  拿到result 并输出
-        System.out.println("异步计算结果为："+result);
-         
-        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
-        
-        // 然后退出main线程
+/*    public static void main(String[] args) throws Exception {
+        long start = System.currentTimeMillis();
+
+        new Thread(() -> {
+            int result =sum();
+            System.out.println("线程执行结果："+result);
+        }).start();
+        Thread.sleep(3000);
+        System.out.println("main线程结束。。。");
+    }*/
+//第二种方法
+/*public static void main(String[] args) throws Exception {
+    Thread thread = new Thread(() -> {
+        int result = sum();
+        System.out.println("线程执行结果：" + result);
+    });
+    thread.start();
+    thread.join();
+    System.out.println("main线程结束。。。");
+}*/
+
+    /**
+     *第三种方法
+     */
+/*    public static void main(String[] args) throws Exception {
+        Thread thread = new Thread(() -> {
+            int result = sum();
+            System.out.println("线程执行结果：" + result);
+        });
+        Homework03 homework03=new Homework03();
+        thread.start();
+        synchronized (homework03){
+            homework03.wait(400);
+        }
+        System.out.println("main线程结束。。。");
+    }*/
+
+    /**
+     *第四种
+     */
+    public static void main(String[] args) throws Exception {
+        CountDownLatch countDownLatch=new CountDownLatch(2);
+        Thread thread = new Thread(() -> {
+            int result = sum();
+            System.out.println("线程执行结果：" + result);
+        });
+        thread.start();
+        countDownLatch.await(4, TimeUnit.SECONDS);
+        System.out.println("main线程结束。。。");
     }
-    
+
+
     private static int sum() {
         return fibo(36);
     }
-    
+
     private static int fibo(int a) {
-        if ( a < 2) 
+        if ( a < 2)
             return 1;
         return fibo(a-1) + fibo(a-2);
     }
